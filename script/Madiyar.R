5a1164f05c8c40da1ae1daaed6d056384ee012a4
mseiduea <knu22fju@uea.ac.uk>
2023-11-20 12:49:59 +0000

    Updating script to be more professional

--- script/Madiyar.R
+++ script/Madiyar.R
@@ -1,1 +1,12 @@
-# Madiyar script yay
+# Script for 5023Y Group Project----
+## Script author: Madiyar Seidaly
+
+
+# ____________________----
+
+# IMPORTING CLEAN DATA----
+
+source("script/cleaning_data.R")
+
+# ____________________----
+
