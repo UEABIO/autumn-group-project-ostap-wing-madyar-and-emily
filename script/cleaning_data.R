5bbdcae9ccc7f8003ad1a30ade23c20fd8086e2a
Romanos001 <ostapykrop1991@gmail.com>
2023-11-20 12:07:43 +0000

    My second commit, so we can work on the read data, also packages added to clean any varibales

--- script/cleaning_data.R
+++ script/cleaning_data.R
@@ -1,1 +1,12 @@
 # script for cleaning
+
+#___________________________----
+# SET UP ----
+
+# PACKAGES ----
+library(tidyverse) # tidy data packages
+library(janitor) # cleans variable names
+
+
+# IMPORT DATA ----
+covid_data_raw <- read_csv("data/owid-covid-data.csv")
