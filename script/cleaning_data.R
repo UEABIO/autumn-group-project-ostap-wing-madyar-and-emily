ecc172693d69d088352c8be9d3847c7eeaad595f
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-11-20 12:14:36 +0000

    installed colour blindness package for accessibility

--- script/cleaning_data.R
+++ script/cleaning_data.R
@@ -8,7 +8,7 @@ library(tidyverse) # tidy data packages
 library(janitor) # cleans variable names
 library(lubridate) # make sure dates are processed properly
 library(patchwork) # To make final figure
-
+library(colorBlindness) #check plot accessibility
 
 # IMPORT DATA ----
 covid_data_raw <- read_csv("data/owid-covid-data.csv")
