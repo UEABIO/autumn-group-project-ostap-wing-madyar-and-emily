81261a0f7af5bf46ad3525fb467c4464b83ebefb
Wing Lun Lim <uyj22nnu@uea.ac.uk>
2023-11-20 12:51:05 +0000

    intital code for personal script

---
+++ script/Wing.R
@@ -0,0 +1,14 @@
+#___________________________----
+# SET UP ----
+## An analysis of the  ----
+
+### Data first published in  Batra, Neale et al. (2021), The Epidemiologist R Handbook. ------
+#__________________________----
+# PACKAGES ----
+library(tidyverse) # tidy data packages
+library(janitor) # cleans variable names
+library(lubridate) # make sure dates are processed properly
+library(patchwork) # To make final figure
+library(colorBlindness) #check plot accessibility
+
+#__________________________----
