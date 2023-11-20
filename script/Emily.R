d9d8811da1bde9ff1ab6c79d412530ca51d0ae36
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-11-20 12:50:53 +0000

    set up script with clear outline

--- script/Emily.R
+++ script/Emily.R
@@ -1,1 +1,14 @@
-## Emily's script
+# Emily's script
+
+# Load R Objectve and Functions----
+source("script/cleaning_data.R") #imported tidied covid data and functions
+
+#_____________________----
+
+# Create Plot ----
+
+#_____________________----
+
+# Check Accessibility----
+## library(colorBlindness)
+## colorBlindness::cvdPlot()
