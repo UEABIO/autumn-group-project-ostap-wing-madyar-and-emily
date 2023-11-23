649535535edfb04565c97e2b6f0e190937b006bf
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-11-23 16:35:14 +0000

    decided what variables to explore

--- script/Emily.R
+++ script/Emily.R
@@ -1,14 +1,24 @@
-# Emily's script
+# BIO-5023Y Autumn Summative Group Project Script---- 
+## Tsz-ching Chiu (Emily)
+### Topic: Exploring confirmed cases and symptoms
 
-# Load R Objectve and Functions----
-source("script/cleaning_data.R") #imported tidied covid data and functions
+# SET UP----
+library(tidyverse) # core packages for data tidying, wrangling and visualisation
+library(janitor) # clean variable names
+library(patchwork) # compile figure, might remove
+library(colorBlindness) #check plot accessibility
+
+#Load R Objectve and Functions----
+source("script/cleaning_data.R") #imported tidied/clean data and functions
 
 #_____________________----
 
+# FILTER----
+#covid_symp_case_df <- covid_data_raw %>%
+#  select()
 # Create Plot ----
 
 #_____________________----
 
 # Check Accessibility----
-## library(colorBlindness)
 ## colorBlindness::cvdPlot()
