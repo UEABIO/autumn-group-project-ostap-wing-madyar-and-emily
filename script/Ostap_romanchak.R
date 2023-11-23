3c1cafefdbdd8c7610712bcef93d8544d0b5cf87
Romanos001 <ostapykrop1991@gmail.com>
2023-11-23 16:36:50 +0000

    For clear understanding whos scripts it is and what it will be doing. Pasted code from clean script to get started working and made it more explanatory and clean

--- script/Ostap_romanchak.R
+++ script/Ostap_romanchak.R
@@ -1,1 +1,19 @@
-## My script: Ostap 
+# Title: BIO-5023Y 23-24 autumn group project ----
+## Ostap
+### Exploring confirmed cases vs death from covid
+
+#__________________----
+# SET UP: Loading packages ----
+
+library(tidyverse) # tidy data packages
+library(janitor) # cleans variable names
+library(patchwork) # To make final figure
+library(colorBlindness) #check plot accessibility
+
+#__________________-----
+
+# Importing the data ----
+covid_data_raw <- read_csv("data/covid_example_data.csv")
+#__________________------
+
+
