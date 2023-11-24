a254902d9df0b988280df0d822680dbeb7857214
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-11-24 20:30:12 +0000

    simplified variable names and explored frequencies and distinct values

--- script/Emily.R
+++ script/Emily.R
@@ -1,5 +1,5 @@
 # BIO-5023Y Autumn Summative Group Project Script---- 
-## Tsz-ching Chiu (Emily)
+## Tsz-Ching Chiu (Emily)
 
 ### Data first published in Batra, Neale et al. (2021), The Epidemiologist R Handbook. ------
 ### Figure topic: Explore how symptoms indicate COVID-19 diagnosis
@@ -21,14 +21,51 @@ source("script/cleaning_data.R") #from separate script for cleaning
 # EXPLORATORY ANALYSIS----
 
 ## Select relevant variables----
-covid_symp_case <- select(.data = covid_data_raw, #data from cleaning script
+symp_case <- select(.data = covid_data_raw, #data from cleaning script
                           confirmed_case, sym_fever, sym_subjfever, sym_myalgia, sym_losstastesmell, 
                           sym_sorethroat, sym_cough, sym_headache)
+#query source of sym_subjfever and sym_cough
 
-# Consider filter or adjust into counts
-# column = positive / negative cases
-# rows = various symptoms
-# cells = indicate count
+## Overview of variables----
+glimpse(symp_case) 
+summary(symp_case)
+### All variables are categorical nominal variables.
+
+### Original frequency for each variable saved as separate object----
+case_n <- symp_case %>% 
+  group_by(confirmed_case) %>% 
+  summarise(n = n()) #output for confirmed cases
+
+fever_n <- symp_case %>%
+  group_by(sym_fever) %>%
+  summarise(n=n()) #output for fever symptoms
+
+myalagia_n <- symp_case %>%
+  group_by(sym_myalgia) %>%
+  summarise(n=n()) #output for myalagia symptoms
+
+loststastesmell_n <- symp_case %>%
+  group_by(sym_losstastesmell) %>%
+  summarise(n=n()) #output for loss of taste and smell
+
+sorethroat_n <- symp_case %>%
+  group_by(sym_sorethroat) %>%
+  summarise(n=n()) #output for sore throat
+
+headache_n <- symp_case %>%
+  group_by(sym_sorethroat) %>%
+  summarise(n=n()) #output for headache
+
+### Frequency for cough and subjfever (N/A in README)
+cough_n <- symp_case %>%
+  group_by(sym_cough) %>%
+  summarise(n=n()) #output for cough
+
+subjfever_n <- symp_case %>%
+  group_by(sym_subjfever) %>%
+  summarise(n=n()) #output for subjfever
+  
+## each variable has 'yes', 'no', 'Na'; confirmed case also with 'pending', symptoms also with 'unk' for 'unknown'
 
 #_____________________----
 # Create Plot ----
