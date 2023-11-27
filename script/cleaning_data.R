316833daf8bdc002f5115a36c8195fcdfebe7c67
Romanos001 <ostapykrop1991@gmail.com>
2023-11-27 12:00:09 +0000

    After group discussion, decided to remove redundant and improve presentation of new data

--- script/cleaning_data.R
+++ script/cleaning_data.R
@@ -1,10 +1,26 @@
-# script for cleaning
+# Script for cleaning
 
 #___________________________----
 # SET UP ----
 
 # PACKAGES ----
 library(tidyverse) # tidy data packages
+library(janitor) # clean varibale names
 
 # IMPORT DATA ----
 covid_data_raw <- read_csv("data/covid_example_data.csv")
+#___________________________----
+
+
+## Exploring data to clean it -----
+
+# Checking for NA and varibale names
+summary(covid_data_raw)
+
+# Cleaning all variable names
+
+covid_data <- janitor::clean_names(covid_data_raw)
+
+#Checking variable names
+glimpse (covid_data)
+
