3cc637348dde447434fa1e16f295d3ad3e662ebe
Wing Lun Lim <uyj22nnu@uea.ac.uk>
2023-11-27 12:01:30 +0000

    removed redundant code from personal script, make script more presentable

--- script/Wing.R
+++ script/Wing.R
@@ -1,14 +1,16 @@
 #___________________________----
 # SET UP ----
-## An analysis of the  ----
+## An analysis and plot of covid data - gender vs covid deaths  ----
 
 ### Data first published in  Batra, Neale et al. (2021), The Epidemiologist R Handbook. ------
 #__________________________----
 # PACKAGES ----
+
 library(tidyverse) # tidy data packages
-library(janitor) # cleans variable names
-library(lubridate) # make sure dates are processed properly
 library(patchwork) # To make final figure
 library(colorBlindness) #check plot accessibility
 
+#__________________________----
+# Import tided data and load r objects and functions 
+source("scripts/cleaning_data.R")
 #__________________________----
