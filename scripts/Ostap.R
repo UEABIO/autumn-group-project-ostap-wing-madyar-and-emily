de2f2fea25fd38092762eefd428ed04f2a87942c
Romanos001 <ostapykrop1991@gmail.com>
2023-12-11 15:22:39 +0000

    Important to inform audience across what period the study was conducted

--- scripts/Ostap.R
+++ scripts/Ostap.R
@@ -124,8 +124,8 @@ age_death_bar_plot <- age_vs_covid_death %>%      # Saving my plot
             colour= "black") +      #Setting width of bars
   labs(x="Age groups \n",       # Labeling x-axis and making an indent for clarity
        y = "\n Number of deaths per 40 000",      # Labeling y-axis and making an indent for clarity
-       title= "Number of COVID19 deaths by age categories", # Inserting a tittle
-       subtitle= "Proportions of individual groups") +       # Inserting a sub-tittle
+       title= "The number of COVID19 deaths in the U.S. by the age groups", # Inserting a tittle
+       subtitle= "Study conducted between 27th December 2019 and 27th July 2021") +       # Inserting a sub-tittle
   geom_text(data=age_death_stats,   # Providing data for bar text
             aes(y=(n+35),       # Shifting bar text to make is visually accessible
                 x=age_range,        # Stating where I like my bar text to be
