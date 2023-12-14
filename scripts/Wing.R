4a2c2a440d2eaefde9660e877ed88909cdf7f1b1
Wing Lun Lim <uyj22nnu@uea.ac.uk>
2023-12-14 01:37:32 +0000

    making script sequential allowing for easier read and understanding. removing redundant code.

--- scripts/Wing.R
+++ scripts/Wing.R
@@ -1,37 +1,17 @@
-#___________________________----
-# SET UP ----
-## An analysis and plot of covid data - gender vs covid deaths  ----
+# Title: BIO-5023Y 23-24 autumn group project ----
+## Wing Lun Lim
+### An analysis and plot of covid data - gender vs covid deaths
+#### Data first published in  Batra, Neale et al. (2021), The Epidemiologist R Handbook
 
-### Data first published in  Batra, Neale et al. (2021), The Epidemiologist R Handbook. ------
-#__________________________----
-# PACKAGES ----
-library(colorBlindness) #check plot accessibility
-#__________________________----
-# Import tided data and load r objects and functions 
-source("script/cleaning_data.R")
-#__________________________----
-# DPLYR VERBS ----
-# Checking distinct ouputs
-covid_data%>%
-  distinct(case_gender, died_covid)
-           
-covid_data%>%
-  distinct(case_gender) # 4 distinct outputs Male, Female, NA, Unknown
+#___________________----
 
-covid_data%>%
-  distinct(died_covid) # 4 distinct outputs No, NA, Yes, Under Review 
+# Setting up loading packages ----
+library(colorBlindness) #check accessibility of plot to colour blind users
 
-# Filtered for only people who died from covid
-gender_vs_covid_death <- covid_data %>% 
-  select(pid, case_gender, died_covid) %>% 
-  filter(died_covid == "Yes") 
+#___________________-----
 
-# check for duplicate rows in the data
-gender_vs_covid_death %>% 
-  duplicated() %>% # produces a list of TRUE/FALSE statements for duplicated or not
-  sum() # sums all the TRUE statements
+# Loading objects and functions from main cleaning script ----
+# Import tidied covid data
 
-# checking for typos in answers
-gender_vs_covid_death %>%
-  distinct(case_gender, died_covid)
+source("scripts/cleaning_data.R")
 
