3fbf91236754802b99b2d4fcc58cd2b27497eea0
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-12-21 08:12:45 +0000

    adjusted script comment for Emily's plot to be more concise

--- scripts/Emily.R
+++ scripts/Emily.R
@@ -1,24 +1,24 @@
 # BIO-5023Y Autumn Summative Group Project Script---- 
 ## Tsz-Ching Chiu (Emily)
-### Topic: In confirmed cases, how indicative is the symptom of headache?
+### Topic: How indicative is the symptom of headache for a positive COVID-10 diagnosis?
 
 #_____________________----
 
 # SET UP----
 ## Packages----
-library(tidyverse)
-library(colorBlindness) #check accessibility of plots by other teammates
+library(tidyverse) #for data analysis and plotting
+library(colorBlindness) #check plot accessibility
 
 ## Load R Objects and Functions----
-source("scripts/cleaning_data.R") #from separate script for cleaning
+source("scripts/cleaning_data.R") #load cleaned data from separate script
 
 #_____________________----
 
 # REVIEW----
 ## select variables from original data----
 covid_data %>%
-  group_by(confirmed_case, sym_headache) %>%  # Grouping data by the 2 variables
-  summarise(n = n()) # Outputting all different values per variable, include NAs
+  group_by(confirmed_case, sym_headache) %>%  # Group data by the 2 variables
+  summarise(n = n()) # Output all different values per variable, including NAs
 
 ##_____________________----
 
@@ -31,9 +31,9 @@ covid_data %>%
 
 ## Check for total number of positive cases----
 covid_data %>%
-  select(confirmed_case)%>% # Selecting cases variable from original dataframe
+  select(confirmed_case)%>% # Selecting cases variable from cleaned data
   filter(confirmed_case == "Yes") %>% #Filter only positive cases
-  summarise(n=n()) #calculate the count: 82061
+  summarise(n=n()) #count: 82061
 
 ##_____________________----
 
@@ -43,15 +43,15 @@ covid_data %>%
   summarise(n=n(),
             n_distinct=n_distinct(pid)) %>% #number of unique cases
   ungroup() %>% #remove group calculations
-  mutate(freq=n_distinct/sum(n_distinct)) %>% # calculate percentage of each group across the dataset
+  mutate(freq=n_distinct/sum(n_distinct)) %>% # calculate percentage of each group
   mutate()
-# By comparing n and n_distinct, some repeated values are observed
+#Difference in values for n and n_distinct indicates repeated observations
 
 #_____________________----
 
 #SUBSET OF DATA----
 sym_case <- select(.data = covid_data, 
-                   c(pid, confirmed_case, sym_headache)) #select variables of interest
+                   c(pid, confirmed_case, sym_headache)) #select variables
 
 sym_case <- sym_case %>%
   filter(confirmed_case %in% c("Yes")) %>% #keep only positive covid results
@@ -62,13 +62,13 @@ head(sym_case) #review
 
 ##_____________________----
 
-## Check the frequency of positive and negative cases----
+## Check the frequency for different status of headache reported----
 sym_case %>% 
   group_by(headache) %>% 
   summarise(n=n(),
             n_distinct=n_distinct(pid)) %>% #number of unique cases
   ungroup() %>% #remove group calculations
-  mutate(freq=n/sum(n)) %>% # calculate percentage of each group across the dataset
+  mutate(freq=n/sum(n)) %>% # calculate percentage of each group
   mutate()
 #Difference in values for n and n_distinct indicates repeated observations
 
@@ -93,7 +93,7 @@ sym_case_grp_n <- sym_case_grp %>%
   mutate()
 
 sym_case_grp_n <-sym_case_grp_n %>%
-  mutate(freq = round(freq, digits = 2)) %>% #round off the relative frequency to 3 digits
+  mutate(freq = round(freq, digits = 2)) %>% #round off the relative frequency
   mutate(percent = freq * 100) #obtain percentage
 
 sym_case_grp_n <- sym_case_grp_n %>%
@@ -105,22 +105,22 @@ sym_case_grp_n <- sym_case_grp_n %>%
 
 ## Preparation----
 total <-sym_case_grp_n %>%
-  summarise(total = sum(n))%>% #obtain value of the total number of positive cases
-  pull() #extract the numerical value from the tibble
+  summarise(total = sum(n))%>% #obtain the numerical value of the total number of positive cases
+  pull() #extract the number from the tibble
 
 ##_____________________----
 
 ## Create Bar Plot----
 headache_bar <- sym_case_grp_n %>% #save the plot
   ggplot(aes(x=n_distinct, y=headache, fill = headache))+ #select variables
-  geom_col(width = 0.6, colour = "black")+ #adjust width of the bars and apply borders to make them more visible
+  geom_col(width = 0.6, colour = "black")+ #adjust bar width and apply borders to increase visibility
   xlim(0, 35000)+
   labs(x = "Number of cases",
        y = "Symptom of headache experienced", #provide labels for both axes
        title = "The symptom of headache does not indicate a positive COVID-19 diagnosis",
-       subtitle = paste0("Sample Size: ", total," positive cases"))+ #combine the variable for total number of positive cases into the string
-  geom_text(aes(label = scales::percent(freq)), hjust = -0.15)+ #display percentage
-  scale_fill_manual(values=c("darkblue", #Apply colours to the corresponding categories to highlight the unconfirmed cases
+       subtitle = paste0("Sample Size: ", total," positive cases"))+ #combine the numerical value with the string
+  geom_text(aes(label = scales::percent(freq)), hjust = -0.15)+ #display percentage with the location adjusted
+  scale_fill_manual(values=c("darkblue", #Apply colours to the corresponding categories
                              "deepskyblue4",
                              "deepskyblue"))+
   theme_classic()+
@@ -134,8 +134,8 @@ colorBlindness::cvdPlot()
 ##_____________________----
 
 ## Output----
-ggsave("figures/emily_headache_barplot.png", # Assign folder and name to the file
-       plot= headache_bar, # Assign my plot to be saved
+ggsave("figures/emily_headache_barplot.png", # Assign the folder and file name
+       plot= headache_bar, # Assign the plot to be saved
        dpi=300, # Setting resolution
        width = 8, #Setting width
        height= 6) #Setting width
