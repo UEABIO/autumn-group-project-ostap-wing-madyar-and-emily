bc8700e3ccae49220c4cc21abaaa9da29d1d906a
Emily-Chiu-TC <emilyuk2022@gmail.com>
2023-12-21 09:07:35 +0000

    tidied Madiyar's scripts and adjusted the comments

--- scripts/Madiyar.R
+++ scripts/Madiyar.R
@@ -4,12 +4,10 @@
 
 # ____________________----
 
-# IMPORTING CLEAN DATA----
-
+# IMPORTING OBJECTS AND PACKAGES----
 source("scripts/cleaning_data.R") # Importing cleaned up data
 
 library(patchwork) # Loading the patchwork package
-
 library(ggplot2) # Loading the ggplot2 package
 
 # ____________________----
@@ -17,12 +15,10 @@ library(ggplot2) # Loading the ggplot2 package
 # FURTHER CLEANING----
 
 # Selecting only symptom and hospitalization data
-
 symptom_data <- select(.data = covid_data,
                        sym_fever, sym_myalgia, sym_losstastesmell, sym_sorethroat, sym_cough, sym_headache, hospitalized)
 
 # Renaming variables to be easier to work with
-
 symptom_data <- rename(symptom_data,
                        "sym_fev" = "sym_fever",
                        "sym_mya" = "sym_myalgia",
@@ -32,18 +28,15 @@ symptom_data <- rename(symptom_data,
                        "sym_hdc" = "sym_headache")
 
 # Renaming "Unk" values to "NA"
-
 is.na(symptom_data) <- symptom_data == "Unk"
 
 # Renaming "YES" values to "Yes" in the "sym_mya" column
-
 symptom_data <- symptom_data %>%
   mutate(sym_mya = case_when(sym_mya == "YES" ~ "Yes",
                              sym_mya == "Yes" ~ "Yes",
                              sym_mya == "No" ~ "No"))
 
 # Removing NA data
-
 symptom_data <- drop_na(symptom_data)
 
 # ____________________----
@@ -258,84 +251,80 @@ symptom_data %>%      # Counting All instances of Sore Throats + Not Hospitalize
 # Creating objects for a tibble
 
 # creating the order of symptoms
-
 symptom <- c("fev", "fev", "mya", "mya", "lts", "lts", "cog", "cog", "hdc", "hdc", "sot", "sot")
 
 # signifying if the value is for the symptom on its own or alongside others
-
 n_pos <- c("solo", "multi", "solo", "multi", "solo", "multi", "solo", "multi", "solo", "multi", "solo", "multi")
 
 # manually inputting the counts for rows with Yes values for hospitalization
-
 hosp_Y <- c(53, 680, 29, 728, 38, 547, 132, 838, 21, 621, 11, 354)
 
 # manually inputting the counts for rows with No values for hospitalizations
-
 hosp_N <- c(502, 6715, 452, 10107, 1724, 9484, 956, 10032, 813, 10803, 335, 6109)
 
-# creating the tibble for data with hospitalizations 
-
+# creating the tibble for data with hospitalizations
 sym_count_Y <- tibble(symptom, n_pos, hosp_Y)
 
 # creating the tibble for data without hospitalizations
-
 sym_count_N <- tibble(symptom, n_pos, hosp_N)
 
 # creating factors based on the value of cases with hospitalizations
-
 sym_count_Y <- sym_count_Y %>%
   mutate(symptom = fct_relevel(symptom, "sot", "hdc", "mya", "lts", "fev", "cog"))
 
 # creating labels for each symptom in accordance with the factors
-
 Y_labs <- c("Sore Throat", "Headache", "Myalgia", "Loss of Taste and Smell", "Fever", "Cough")
 
 # creating factors based on the value of cases without hospitalizations
-
 sym_count_N <- sym_count_N %>%
   mutate(symptom = fct_relevel(symptom, "sot", "mya", "fev", "hdc", "cog", "lts"))
 
 # creating labels for each symptom in accordance with the factors
-
 N_labs <- c("Sore Throat", "Myalgia", "Fever", "Headache", "Cough", "Loss of Taste and Smell")
 
 # ____________________----
 
-# BUILDING THE FIGURE----
+# BUILDING THE FIGURE WITH GGPLOT2----
 
-# creating the plot with hospitalizations with the ggplot2 function
-
-plot_Y <- ggplot(sym_count_Y, aes(fill = n_pos, y = symptom, x = hosp_Y))+      # defining the variables to be used as parameters
-  geom_col(position ='stack', width = 1.0, color = "black", show.legend = FALSE)+     # selecting a stacked bar-plot with borders, removing the legend as it will be shown on the second plot
-  scale_fill_manual(values = c("deepskyblue", "darkblue"))+     # adding color
-  scale_y_discrete(labels = Y_labs)+      # providing the Y axis labels for the symptoms
-  labs(x = "Number of cases that led to Hospitalizations",      # labeling the axis of the plot, providing a title and subtitle 
+# creating the plot with hospitalizations
+plot_Y <- ggplot(sym_count_Y, 
+                 aes(fill = n_pos, 
+                     y = symptom, 
+                     x = hosp_Y))+ # defining the variables to be used as parameters
+  geom_col(position ='stack', 
+           width = 1.0, 
+           color = "black", 
+           show.legend = FALSE)+ # selecting a stacked bar-plot with borders, removing the legend as it will be shown on the second plot
+  scale_fill_manual(values = c("deepskyblue", "darkblue"))+ # adding color
+  scale_y_discrete(labels = Y_labs)+ # providing the Y axis labels for the symptoms
+  labs(x = "Number of cases that led to Hospitalizations", # labeling the plot axes
        y = "Symptom",
-       title = "Occurrence of COVID-19 Symptoms leading to hospitalizations",
+       title = "Occurrence of COVID-19 Symptoms leading to hospitalizations", #providing a title and subtitle 
        subtitle = "Coughing symptoms were present in the majority of hospitalizations")+
   theme_classic()     # setting a plot theme
 
-# creating the plot without hospitalizations with the ggplot2 function
-
-plot_N <- ggplot(sym_count_N, aes(fill = n_pos, y = symptom, x = hosp_N))+      # defining the variables to be used as parameters
-  geom_col(position ='stack', width = 1.0, colour = "black")+     # selecting a stacked bar-plot with borders
-  scale_fill_manual(labels = c("With additional Symptoms", "Only specific Symptom"), values = c("deepskyblue", "darkblue"))+      # labeling the legend, adding color
-  scale_y_discrete(labels = N_labs)+      # providing the Y axis labels for the symptoms
-  labs(x = "Number of cases that did not lead to Hospitalizations",      # labeling the axis of the plot and titling the legend, title omitted as it is already included in the first plot
+# creating the plot without hospitalizations
+plot_N <- ggplot(sym_count_N, 
+                 aes(fill = n_pos, 
+                     y = symptom, 
+                     x = hosp_N))+  # defining the variables to be used as parameters
+  geom_col(position ='stack', 
+           width = 1.0, 
+           colour = "black")+  # selecting a stacked bar-plot with borders
+  scale_fill_manual(labels = c("With additional Symptoms", "Only specific Symptom"), 
+                    values = c("deepskyblue", "darkblue"))+  # labeling the legend, adding color
+  scale_y_discrete(labels = N_labs)+  # providing the Y axis labels for the symptoms
+  labs(x = "Number of cases that did not lead to Hospitalizations",  # labeling the axes and titling the legend, title omitted as it is already included in the first plot
        y = "",
        fill = "Included Symptoms")+
   theme_classic()     # setting a plot theme
 
 # patching together the plots with the patchwork function
-
 plot_F <- (plot_Y+plot_N)
 
 # saving the plot
-
 ggsave("figures/madiyar_symptom_barplot.png",
        plot = plot_F,
        dpi = 300,
        width = 21,
        height = 9)
-  
-
